9b96ed2e0705fa6c10f9f64adba2e828ffc5c9f9
sabias <dimabrkvsk29@gmail.com>
2020-10-19 10:26:07 +0300

    11-15

---
+++ LegkiySposVyuchitPython3/ex13.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, fist, second, third = argv
+print("Этот сценарий называется:", script)
+print("Это моя первая переменная называется:", fist)
+print("Это моя вторая переменная называется:", second)
+print("Это моя третья переменная называется:", third)
---
+++ LegkiySposVyuchitPython3/ex15.py
@@ -0,0 +1,15 @@
+# Импорт модуля Sys его библиотеки argv
+from sys import argv
+# команда которой присваиваем имя для библиотеки argv
+script, filename = argv
+# Новая переменная txt с указанным именем который передали в командную строку
+txt = open (filename)
+# Печать содержимого filename
+print(f'Содержание файла {filename}:')
+# Чтение файла без условия
+print(txt.read(), 'r')
+
+print('Снова введите имя файла:')
+file_again = input('> ')
+txt_again = open(file_again)
+print(txt_again.read())
